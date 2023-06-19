e99ad59cb73f5c094d2bc5fa815807cbaa573703
Carlos R. Mercado <107061601+charlieflipside@users.noreply.github.com>
2023-06-19 18:04:21 -0400

    add frETH and FRZ

---
+++ src/components/FRZBalance.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import { account, contract } from "sveeeth";
+    import { FRZ_CONFIG } from "../constants";
+
+    const frz = contract(FRZ_CONFIG);
+</script>
+
+    <h3>FRZ Balance</h3>
+    {#await $account.address && frz.balanceOf($account.address)}
+    <p>Loading...</p>
+    {:then balance}
+    <p>Balance: {balance/1e18} FRZ</p>  
+    {/await}
---
+++ src/components/frETHBalance.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import { account, contract } from "sveeeth";
+    import { FRETH_CONFIG } from "../constants";
+
+    const freth = contract(FRETH_CONFIG);
+</script>
+
+    <h3>freth Balance</h3>
+    {#await $account.address && freth.balanceOf($account.address)}
+    <p>Loading...</p>
+    {:then balance}
+    <p>Balance: {balance/1e18} frETH</p>  
+    {/await}
