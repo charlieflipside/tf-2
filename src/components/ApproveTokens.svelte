33484ebfbe931a0ab27cb0a81848f39c2159d0ca
Carlos R. Mercado <107061601+charlieflipside@users.noreply.github.com>
2023-08-06 20:00:59 -0400

    Check & Request Approvals

--- src/components/ApproveTokens.svelte
+++ src/components/ApproveTokens.svelte
@@ -12,19 +12,85 @@ export const weth = contract(WETH_CONFIG);
 export const frz = contract(FRZ_CONFIG);
 export const freezer = contract(NFT_CONFIG);
 export const governorContract = contract(GOV_CONFIG);
-let approvalAmount: number;
+let govWethAllowance: number;
+let govFrethAllowance: number;
+let govNFTAllowance: boolean;
 
 $: async () => {
     if ($account.address) {
-    const rawApprovalAmount = await checkGovernorWETHApproval();
-    approvalAmount = parseFloat(ethers.utils.formatUnits(rawApprovalAmount, 18));
+    const rawGovWethAllowance = await checkGovernorWETHApproval();
+    govWethAllowance = parseFloat(ethers.utils.formatUnits(rawGovWethAllowance, 18));
+    
+    const rawGovFrethAllowance = await checkGovernorWETHApproval();
+    govFrethAllowance = parseFloat(ethers.utils.formatUnits(rawGovFrethAllowance, 18));
+    
+    govNFTAllowance = await checkGovernorNFTApproval();
+
     }
 }
 
-const getApprovalAmount = async () => {
-    const rawApprovalAmount = await checkGovernorWETHApproval();
-    approvalAmount = parseFloat(ethers.utils.formatUnits(rawApprovalAmount, 18));
+/* Gets */
+
+const getGovWethAllowance = async () => {
+    const rawGovWethAllowance = await checkGovernorWETHApproval();
+    govWethAllowance = parseFloat(ethers.utils.formatUnits(rawGovWethAllowance, 18));
 };
+
+const getGovFrethAllowance = async () => {
+    const rawGovFrethAllowance = await checkGovernorfrETHApproval();
+    govFrethAllowance = parseFloat(ethers.utils.formatUnits(rawGovFrethAllowance, 18));
+};
+
+const getGovNFTAllowance = async () => {
+    govNFTAllowance = await checkGovernorNFTApproval();
+};
+
+export const checkGovernorNFTApproval = async () => {
+    try {
+        if (!$account.address) {
+        alert("Please connect your wallet first.");
+        return;
+        }
+
+        // Get the user's address
+        const userAddress = $account.address;
+
+        // Check if the user has set approval for all tokens for the Governor contract
+        const isApproved = await freezer.isApprovedForAll(userAddress, NFT_CONFIG.address);
+
+        // Log the result
+        console.log(`NFT Approval for Governor contract: ${isApproved}`);
+        return isApproved;
+    } catch (error) {
+        alert("Error: " + error.message);
+        throw error;
+    }
+};
+
+/* Checks */
+
+export const checkGovernorfrETHApproval = async() => {
+
+    try {
+    if (!$account.address) {
+    alert("Please connect your wallet first.");
+    return;
+    }
+
+    // Get the user's address
+    const userAddress = $account.address;
+
+    // Check the allowance for the Governor contract
+    const allowance = await freth.allowance(userAddress, GOV_CONFIG.address);
+
+     // Log the allowance
+    console.log(`frETH Allowance for Governor contract: ${allowance}`);
+    return allowance;
+} catch (error) {
+    alert("Error: " + error.message);
+    throw error;
+    }
+}
 
 export const checkGovernorWETHApproval = async () => {
 
@@ -41,7 +107,7 @@ export const checkGovernorWETHApproval = async () => {
     const allowance = await weth.allowance(userAddress, GOV_CONFIG.address);
 
     // Log the allowance
-    console.log(`Allowance for Governor contract: ${allowance}`);
+    console.log(`WETH Allowance for Governor contract: ${allowance}`);
     return allowance;
 } catch (error) {
     alert("Error: " + error.message);
@@ -49,30 +115,32 @@ export const checkGovernorWETHApproval = async () => {
 }
 };
 
+/* Approvals */
+
 export const approveWETHtoGovernor = async () => {
 
-        try {
-            if (!$account.address) {
-            alert("Please connect your wallet first.");
-            return;
-        }
-    
-            // Get the user's WETH balance
-            const userAddress = $account.address;
-            const balance = await weth.balanceOf(userAddress);
+    try {
+        if (!$account.address) {
+        alert("Please connect your wallet first.");
+        return;
+    }
 
-            // Approve the Governor contract to spend the user's WETH balance
-            const transaction = await weth.approve(GOV_CONFIG.address, balance);
+        // Get the user's WETH balance
+        const userAddress = $account.address;
+        const balance = await weth.balanceOf(userAddress);
 
-            // Wait for the transaction to be mined
-            await transaction.wait();
+        // Approve the Governor contract to spend the user's WETH balance
+        const transaction = await weth.approve(GOV_CONFIG.address, balance);
 
-            // Transaction successfully mined
-            console.log("Transaction successful!");
-        } catch (error) {
-            alert("Error: " + error.message);
-        }
-    };
+        // Wait for the transaction to be mined
+        await transaction.wait();
+
+        // Transaction successfully mined
+        console.log("Transaction successful!");
+    } catch (error) {
+        alert("Error: " + error.message);
+    }
+};
 
 export const approvefrETHtoGovernor = async () => {
     try {
@@ -121,6 +189,8 @@ export const approveAllNFTsToGovernor = async () => {
 
 </script>
 
+<!-- Buttons -->
+
 <h2>Approve Governor to use WETH</h2>
 <button on:click={approveWETHtoGovernor}>Approve WETH to Governor</button>
 
@@ -131,5 +201,13 @@ export const approveAllNFTsToGovernor = async () => {
 <button on:click={approveAllNFTsToGovernor}>Approve Freezer NFT to Governor</button>
 
 <h2>Gov WETH Approval </h2>
-<p>Approval Amount: {approvalAmount}</p>
-<button on:click={getApprovalAmount}>Check Approval</button>
+<p>Approval Amount: {govWethAllowance}</p>
+<button on:click={getGovWethAllowance}>Check Approval</button>
+
+<h2>Gov frETH Approval </h2>
+<p>Approval Amount: {govFrethAllowance}</p>
+<button on:click={getGovFrethAllowance}>Check Approval</button>
+
+<h2>Gov NFT All Approval </h2>
+<p>Approved for All: {govNFTAllowance}</p>
+<button on:click={getGovNFTAllowance}>Check if Approved for All</button>
